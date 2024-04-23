b647bbdd4a280a7ecbf15bfc520fbb12d017cb5a
Kokila412 <134578250+Kokila412@users.noreply.github.com>
2024-04-23 12:59:53 +0530

    Create mini_project.py
    a mini project to guess a word from the given list in the order of the word's letters

---
+++ mini_project.py
@@ -0,0 +1,21 @@
+import random
+
+list_of_words=["banana","apple","condition","chopper","luffy","durian"
+               ,"tofu","kokila","nail","phone"]
+
+selected_word = random.choice(list_of_words)
+total_chance = len(list_of_words)
+while total_chance>0:
+    fail=0
+    def chance(fail,total_chance):
+        for char in selected_word:
+            a=print(input("enter a char:"))
+            if a==char:
+                print(a)
+            else:
+                print("-")
+                fail += 1
+                total_chance -= 1
+                chance(fail,total_chance)
+else:
+    print("failed")
