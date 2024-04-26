23651cff7515b2d146b49f4f15c0494228571e1d
Kokila412 <134578250+Kokila412@users.noreply.github.com>
2024-04-26 20:28:55 +0530

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -5,15 +5,18 @@ list_of_words=["banana","apple","condition","chopper","luffy","durian"
 
 selected_word = random.choice(list_of_words)
 total_chance = len(list_of_words)
+guessed=""
 while total_chance>0:
     fail=0
-      for char in selected_word:
+    for char in selected_word:
           a=print(input("enter a char:"))
           if a==char:
-              print(a)
+            guessed=guessed+a
+            print(guessed)
           else:
               print("-")
               fail += 1
               total_chance -= 1
-else:
-    print("failed")
+    if total_chance==0:
+        print("failed")
+  
