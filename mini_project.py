ef7099c0bd1e2ba95c29d04bf65a29cd25c370fc
Kokila412 <134578250+Kokila412@users.noreply.github.com>
2024-04-26 22:22:50 +0530

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -6,17 +6,26 @@ list_of_words=["banana","apple","condition","chopper","luffy","durian"
 selected_word = random.choice(list_of_words)
 total_chance = len(list_of_words)
 guessed=""
+print(len(selected_word),"lettered word",selected_word)
 while total_chance>0:
+    a=input("enter a char:")
     fail=0
-    for char in selected_word:
-          a=input("enter a char:")
-          if a==char:
-            guessed=guessed+a
-            print(guessed)
+    count=0
+    temp=1
+    for char in selected_word: 
+          if a in selected_word:
+               count+=1
+          if a == char:
+             guessed=guessed+a
+             print(guessed)
           else:
-              print("-")
-              fail += 1
-              total_chance -= 1
-    if total_chance==0:
-        print("failed")
-  
+              fail=fail+1
+              print("-",fail)
+    if guessed==selected_word:
+              break
+    if fail==len(selected_word):
+        total_chance -= 1
+
+if total_chance==0:
+    print("failed")
+
