29722d2eb9a77067c2bac6b1faed7f76f54aea0c
Kokila412 <134578250+Kokila412@users.noreply.github.com>
2024-04-27 13:28:23 +0530

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -10,22 +10,29 @@ print(len(selected_word),"lettered word",selected_word)
 while total_chance>0:
     a=input("enter a char:")
     fail=0
-    count=0
-    temp=1
-    for char in selected_word: 
-          if a in selected_word:
-               count+=1
-          if a == char:
-             guessed=guessed+a
-             print(guessed)
-          else:
-              fail=fail+1
-              print("-",fail)
-    if guessed==selected_word:
-              break
+    temp=0
+    for i in range(len(selected_word)): 
+           if selected_word[i]==a:
+                if len(guessed)==0:
+                   guessed=guessed+a
+                elif guessed[len(guessed)-1]==selected_word[i-1]:
+                     if temp!=total_chance:
+                      guessed=guessed+a
+           else:
+                print("-")
+                fail=fail+1
     if fail==len(selected_word):
-        total_chance -= 1
-
+         print(total_chance,"chances left")
+         print()
+         temp=total_chance
+         total_chance=total_chance-1
+    else:
+        print(guessed, end=" ")
+        print()
+    if guessed==selected_word:
+       print(guessed)
+       break
+        
 if total_chance==0:
     print("failed")
 
