dcbf2786178af28820783cefaf0bcc47fe95723c
Kokila412 <134578250+Kokila412@users.noreply.github.com>
2024-04-27 20:45:15 +0530

    Update beginner_project.py

--- beginner_project.py
+++ beginner_project.py
@@ -13,28 +13,28 @@ for i in flower:
     guess.append("-")
 for i in guess:
     print(i, end=" ")
-while chances>0:
-	print()
-	ans=input("enter your input:")
-	fail=0
-	if not ans.isalpha():
-		print("please enter an alphabet")
-	if len(ans)>1:
-		print("enter only a single character")
-	for i in range(len(flower)):
-			if flower[i]==ans:
-					guess[i]= flower[i]
-			else:
-				fail=fail+1
-	for i in guess:
-		print(i, end=" ")
-	if "-" not in guess:
-			print()
-			print("congrats")
-			break
-	if fail==len(flower):
-		chances=chances-1
-	if chances==0:
+try:
+	while chances>0:
+		print()
+		ans=input("enter your input:")
+		fail=0
+		if not ans.isalpha():
+			print("please, enter an alphabet")
+		if len(ans)>1:
+			print("enter only a single character")
+		for i in range(len(flower)):
+				if flower[i]==ans:
+						guess[i]= flower[i]
+				else:
+					fail=fail+1
+		for i in guess:
+			print(i, end=" ")
+		if "-" not in guess:
+				print()
+				print("congrats")
+				break
+		if fail==len(flower):
+			chances=chances-1
+except KeyboardInterrupt:
 		print()
 		print("failed")
-		break
