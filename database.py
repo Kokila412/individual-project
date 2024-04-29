e15788ace73598dbaf3b4d7c74f5d67f8eedc3f2
Kokila412 <134578250+Kokila412@users.noreply.github.com>
2024-04-29 21:53:40 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,38 @@
+data = [
+	{
+		'name': 'Instagram',
+		'follower_count': 346,
+		'description': 'Social media platform',
+		'country': 'United States'
+	},
+	{
+		'name': 'Cristiano Ronaldo',
+		'follower_count': 215,
+		'description': 'Footballer',
+		'country': 'Portugal'
+	},
+	{
+		'name': 'Ariana Grande',
+		'follower_count': 183,
+		'description': 'Musician and actress',
+		'country': 'United States'
+	},
+    {
+		'name': 'Facebook',
+		'follower_count': 446,
+		'description': 'Social media platform',
+		'country': 'United States'
+	},
+	{
+		'name': 'BTS',
+		'follower_count': 10000000,
+		'description': 'Footballer',
+		'country': 'Portugal'
+	},
+	{
+		'name': 'Ana Huang',
+		'follower_count': 456,
+		'description': 'Musician and actress',
+		'country': 'United States'
+	}
+]
